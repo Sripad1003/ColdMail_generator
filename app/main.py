65cd3b709c580c220aacd0abfd8fad8527aa01b9
ChiliverySripad <chiliverysripad8@gmail.com>
2025-12-03 18:57:42 +0530

    mail automation

--- app/main.py
+++ app/main.py
@@ -3,11 +3,35 @@ from chains import Chain
 from portfolio import Portfolio
 from utils import clean_text
 from scrapers import scrape_with_fallbacks
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+sender_email = os.getenv("EMAIL_USER")
+sender_password = os.getenv("EMAIL_PASS")
+
+def send_email(receiver_email, subject, body, sender_email, sender_password):
+    msg = MIMEMultipart()
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+    msg["Subject"] = subject
+
+    msg.attach(MIMEText(body, "plain"))
+
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(sender_email, sender_password)
+        server.send_message(msg)
+        server.quit()
+        return True
+    except Exception as e:
+        return str(e)
 
 def create_streamlit_app(llm, portfolio, clean_text):
     st.title("📧 Cold Mail Generator")
 
     url_input = st.text_input("Enter a URL:", value="https://jobs.nike.com/job/R-33460")
+    mail_input=st.text_input("Enter mail : ",placeholder="Enter mail id")
     submit_button = st.button("Submit")
 
     if submit_button:
@@ -33,12 +57,24 @@ def create_streamlit_app(llm, portfolio, clean_text):
                 links = portfolio.query_links(skills)
                 email = llm.write_mail(job, links)
                 st.code(email, language="markdown")
+                result = send_email(
+                    receiver_email=mail_input,
+                    subject=f"Application for {job.get('role', 'the position')}",
+                    body=email,
+                    sender_email=sender_email,
+                    sender_password=sender_password
+                )
+
+                if result is True:
+                    st.success("✔ Email sent successfully")
+                else:
+                    st.error(f"❌ Error: {result}")
 
         except Exception as e:
-            st.error(f"❌ An error occurred: {e}")
+                st.error(f"❌ An error occurred: {e}")
 
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     chain = Chain()
     portfolio = Portfolio()
 
